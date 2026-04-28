7ca76010fb62bd788e96d95d6cac1a5a1c25b73a
Chris Tulino <chris.tulino@gmail.com>
2026-04-28 16:29:19 -0400

    chore(engine): re-export item/board/worker/multitasking

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -2,3 +2,7 @@ export const ENGINE_VERSION = "0.0.1";
 export * from "./types.js";
 export { createPrng, type Prng } from "./prng.js";
 export { sampleLogNormal, sampleSkewNormal, sampleBetaTruncated, samplePoisson } from "./distributions.js";
+export { createItem, isBlocked, advanceItemEffort, resetEffortForColumnTransition } from "./item.js";
+export { columnHasCapacity, currentWorkerLoads, workerCanPull } from "./board.js";
+export { effectiveWorkHours } from "./multitasking.js";
+export { decideWorkerAction, type WorkerAction } from "./worker.js";
