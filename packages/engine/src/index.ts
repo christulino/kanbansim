456a7bebf2e89ff7f8fa44411c0d2f1620630876
Chris Tulino <chris.tulino@gmail.com>
2026-04-28 16:11:23 -0400

    chore(engine): re-export distribution samplers

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,3 +1,4 @@
 export const ENGINE_VERSION = "0.0.1";
 export * from "./types.js";
 export { createPrng, type Prng } from "./prng.js";
+export { sampleLogNormal, sampleSkewNormal, sampleBetaTruncated, samplePoisson } from "./distributions.js";
